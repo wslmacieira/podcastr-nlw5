fc253f07da3225265e50743b8685b22a929c3523
wslmacieira <wslmacieira@outlook.com>
2021-04-23 18:20:49 -0300

    adicionado server.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+// SPA Single Page Application
+// SSR Server Static Rendering 
+// SSG Server Static Generation
 
-export default function Home() {
+export default function Home(props) {
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //     .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, [])
+
+  console.log(props)
+
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
